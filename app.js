2a9bbb90d10b68ff6fc354568af4b640cad2c885
Sara Cashman <saramcashman@gmail.com>
2016-12-13 22:11:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,188 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose   = require('mongoose');
+var router = express.Router();
+var db;
+var Schema       = mongoose.Schema;
+var BookSchema   = new Schema({
+    title: String,
+    author: String,
+    readBook: String,
+    review: String,
+    tags: String,
+    
+});
+
+mongoose.connect('mongodb://Admin1:Password1@ds111178.mlab.com:11178/bookshelf');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
+
+
+
+
+
+var Novel = mongoose.model('Novel', BookSchema);
+
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json());
+
+// add a book
+  app.post('/books', function(req,res){
+
+      book = new Novel({title: req.body.title,
+                author: req.body.author,
+                              readBook: req.body.readName,
+                              review: req.body.infoForm,
+                              tags: req.body.tagsName
+                });
+
+      book.save(function(err) {
+            if (err)
+                res.send(err);
+            console.log(req.body.review)
+            console.log(req.body.tags)
+            //console.log(book)
+            // res.json({ message: 'Book added!' });
+            res.redirect('/');
+        });   
+    });
+    
+// get ALL THE BOOKS
+    app.get('/books', function(req, res) {
+      Novel.find(function(err, books) {
+      if (err)
+        res.send(err);
+      res.json(books);
+    });
+  });
+
+    // get ONE book
+  
+  app.get('/books/:_id', function(req, res) {
+    Novel.findById(req.params._id, function(err, book) {
+      if (err)
+        res.send(err);
+      res.json(book);
+    });
+  })
+
+// delete a book
+  app.delete('/books/:_id', function(req, res){
+    Novel.findByIdAndRemove(req.params._id, function(err, book) {
+        if (err){
+            return res.status(500).json({
+               // message: 'Internal Server Error'
+            });
+        }
+        res.status(201).json({
+            message: 'Item was deleted'
+        })
+    })
+})
+
+//update a book
+app.put('/books/:_id', function(req, res){
+    var queryID = {_id: req.params._id}
+   var updateThis = req.body;
+    // console.log(updateThis)
+    updatedbook = new Novel({title: req.body.title,
+                author: req.body.author,
+                read: req.body.read,
+                review: req.body.review,
+                tags: req.body.tags
+                });
+    console.log(req.params)
+    console.log(req.body)
+    Novel.findOneAndUpdate(queryID, updateThis,
+     function(err, book){
+        if (err){
+            return res.status(500).json({
+               // message: 'Internal Server Error'
+            })
+        }
+        res.status(201).json({
+            message: 'Item was updated'
+        })
+     })
+
+ })
---
+++ public/js/bookshelf.js
@@ -0,0 +1,248 @@
+$(document).ready(function() {
+    var searchTerm = ''
+
+
+    $.getJSON("/books", function(data) {
+
+
+        $.each(data, function(index, value) {
+
+
+            $(".displayExistingTitles").append('<div class="eachAppend" class="' + value.tags + '"><p class=' + value._id + '><span class="bookTitle"> Title: ' + value.title + ' </span><span class="bookAuthor">Author: ' + value.author + ' </span><span class="read">Have you read it? ' + value.readBook + ' </span><span class="bookReview"> notes: ' + value.review + ' </span><span class="bookTags">Tags: ' + value.tags + ' </span><button  id=' + value._id + ' class="deleteButton">Delete</button><button class="editButton">Edit</button><button class="updateButton">Update</button></p></div>')
+
+
+        });
+   
+        $('#showBooks').click(function() {
+            var shelfOption = $('.filterBooks').val();
+            console.log(shelfOption)
+
+            $('.displayExistingTitles div').hide();
+            $('.displayExistingTitles div.' + shelfOption).show();
+
+
+        });
+
+        $('.deleteButton').click(function() {
+            var buttonId = $(this).attr('id');
+            console.log(buttonId)
+            $.ajax({
+                url: "/books/" + buttonId,
+                type: "Delete",
+                success: function() {
+                    $('button#' + buttonId).closest('div').remove();
+                    console.log("item deleted");
+
+                }
+            })
+
+        });
+
+        $('.editButton').click(function() {
+
+
+            $(this).hide();
+            $(this).parent("p").children('.updateButton').show();
+
+            var title = $(this).parent("p").children("span.bookTitle").text();
+            console.log(title)
+            $(this).parent("p").children('span.bookTitle').html("<input id='editTitle' name='editTitle' type='text' value='" + title + "'>");
+
+            var author = $(this).parent("p").children("span.bookAuthor").text();
+            console.log(author)
+            $(this).parent("p").children('span.bookAuthor').html("<input id='editAuthor' name='editAuthor' type='text' value='" + author + "'>");
+
+            var readItText = $(this).parent("p").children("span.read").text();
+            if (readItText == 'yes') {
+                $(this).parent("p").children("span.read").html(' <select name="readName" class="readDropDown"><option value="yes" name="yes" class="readYes" selected>Yes</option><option value="no" name="no" class="readNo">No</option></select>')
+            } else {
+                $(this).parent("p").children("span.read").html(' <select name="readName" class="readDropDown"><option value="no" name="no" class="readNo" selected>No</option><option value="yes" name="yes" class="readYes">Yes</option></select>')
+            }
+
+            var review = $(this).parent("p").children("span.bookReview").text();
+            $(this).parent("p").children('span.bookReview').html("<input id='editReview' name='editReview' type='text' value='" + review + "'>");
+
+            var tagOption = $(this).parent("p").children("span.bookTags").text();
+            if (tagOption == 'To be read') {
+                $(this).parent("p").children("span.bookTags").html(' <select name="tagsName" class="tagsDropDown"><option value="TBR" name="TRB" class="tagsTBR" selected>To be read</option><option value="wishlist" name="wishlist" class="wishList">Wishlist</option><option value="favorites" name="favorites" class="favoritedBook">Favorites</option><option value="reference" name="reference" class="bookReference">Reference</option><option value="readreturn" name="readreturn" class="ReadandReturn">Read & Returned</option></select>')
+            } else if (tagOption == 'Wishlist') {
+                $(this).parent("p").children("span.bookTags").html(' <select name="tagsName" class="tagsDropDown"><option value="wishlist" name="wishlist" class="wishList" selected>Wishlist</option><option value="TBR" name="TRB" class="tagsTBR">To be read</option><option value="favorites" name="favorites" class="favoritedBook">Favorites</option><option value="reference" name="reference" class="bookReference">Reference</option><option value="readreturn" name="readreturn" class="ReadandReturn">Read & Returned</option></select>')
+            } else if (tagOption == 'Favorites') {
+                $(this).parent("p").children("span.bookTags").html(' <select name="tagsName" class="tagsDropDown"><option value="favorites" name="favorites" class="favoritedBook" selected>Favorites</option><option value="wishlist" name="wishlist" class="wishList">Wishlist</option><option value="TBR" name="TRB" class="tagsTBR">To be read</option><option value="reference" name="reference" class="bookReference">Reference</option><option value="readreturn" name="readreturn" class="ReadandReturn">Read & Returned</option></select>')
+            } else if (tagOption == 'Reference') {
+                $(this).parent("p").children("span.bookTags").html(' <select name="tagsName" class="tagsDropDown"><option value="reference" name="reference" class="bookReference" selected>Reference</option><option value="favorites" name="favorites" class="favoritedBook">Favorites</option><option value="wishlist" name="wishlist" class="wishList">Wishlist</option><option value="TBR" name="TRB" class="tagsTBR">To be read</option><option value="readreturn" name="readreturn" class="ReadandReturn">Read & Returned</option></select>')
+            } else {
+                $(this).parent("p").children("span.bookTags").html(' <select name="tagsName" class="tagsDropDown"><option value="readreturn" name="readreturn" class="ReadandReturn" selected>Read & Returned</option><option value="reference" name="reference" class="bookReference" >Reference</option><option value="favorites" name="favorites" class="favoritedBook">Favorites</option><option value="wishlist" name="wishlist" class="wishList">Wishlist</option><option value="TBR" name="TRB" class="tagsTBR">To be read</option><option value="readreturn" name="readreturn" class="ReadandReturn">Read & Returned</option></select>')
+            }
+
+            $('.updateButton').click(function() {
+
+
+                var buttonClassUpdate = $(this).parent('p').attr('class')
+                var editedTitle = $('p.' + buttonClassUpdate).children('span').children('input#editTitle').val();
+                var editedAuthor = $('p.' + buttonClassUpdate).children('span').children('input#editAuthor').val();
+                var editedRead = $('p.' + buttonClassUpdate).children('span.read').children('.readDropDown').val();
+                var editedReview = $('p.' + buttonClassUpdate).children('span').children('input#editReview').val();
+                var editedTags = $('p.' + buttonClassUpdate).children('span.bookTags').children('input#editTags').val();
+
+
+                console.log(editedRead)
+
+
+
+
+                var updateObject = new Object();
+                updateObject._id = buttonClassUpdate;
+                updateObject.title = editedTitle;
+                updateObject.author = editedAuthor;
+                updateObject.readBook = editedRead;
+                updateObject.review = editedReview;
+                updateObject.tags = editedTags
+                console.log(updateObject);
+
+                $.ajax({
+                    url: "/books/" + buttonClassUpdate,
+                    type: "PUT",
+                    data: updateObject,
+                    success: function() {
+
+                        console.log(buttonClassUpdate);
+
+                    }
+
+
+                });
+
+
+            });
+
+        });
+
+    });
+    $('textarea#reviewBox').click(function() {
+        $(this).text('');
+    })
+    $('.addNewTitle h2').click(function() {
+        $('.showAddForm').toggle();
+        if ($(this).text() == 'Add a new title here') {
+            $(this).text('Click to hide form');
+        } else {
+            $(this).text('Add a new title here')
+        }
+
+    });
+
+    $('.suggestionBox').click(function() {
+        $('#suggestSearchBox').show();
+    });
+
+    $('#submitSearch').click(function() {
+        searchTerm = $('#bookSearchTerms').val();
+        console.log(searchTerm)
+
+
+        var params = {
+            q: searchTerm,
+            type: 'books',
+            info: 1,
+            k: '250853-Bookshel-AY9XODQO',
+
+        };
+        url = 'https://www.tastekid.com/api/similar?callback=?&q=' +searchTerm;
+
+
+        $.getJSON(url, params, function(data) {
+            console.log(data);
+            myData = data.Similar.Results
+            console.log(myData)
+      
+
+        $.each(myData, function(index, value) {
+            var newTitle = value.Name
+            var newDescription = value.wTeaser
+
+            $('.showRecommends').append('<p class="recommendedTitle"> '+newTitle+'</p>');
+            $('.showRecommends').append('<p class="recommendedDescription"> '+newDescription+'</p>')
+        });
+          });
+    });
+
+
+// add form modal 
+var addModal = document.getElementById('addTitleModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("openAddButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    addModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    addModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == addModal) {
+        addModal.style.display = "none";
+    }
+}
+// show books modal
+var showModal = document.getElementById('showBooksModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("displayBookshelf");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("showClose")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    showModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    showModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == showModal) {
+        showModal.style.display = "none";
+    }
+}
+
+// show suggestion modal
+var suggestModal = document.getElementById('showSuggestionModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("openSuggestionOverlay");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("suggestClose")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    suggestModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    suggestModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == suggestModal) {
+        suggestModal.style.display = "none";
+    }
+}
+
+});
