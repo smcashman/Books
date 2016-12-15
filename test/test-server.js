c3adbcd79c0c8847c1c6671f97efd269a8d61bb4
Sara Cashman <saramcashman@gmail.com>
2016-12-14 20:47:17 -0500

    add travis

---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(chaiHttp);
+
+describe('books', function() {
+    it('should return status 200', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
